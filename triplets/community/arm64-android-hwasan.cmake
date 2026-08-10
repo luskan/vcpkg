1f0b1d7006fe4824ac234cdd8d9661bd5827e8c7
MarcinJ <marcinje@gmail.com>
2026-08-10 13:17:12 +0200

    Add Android HWASan triplet

---
+++ triplets/community/arm64-android-hwasan.cmake
@@ -0,0 +1,11 @@
+set(VCPKG_TARGET_ARCHITECTURE arm64)
+set(VCPKG_CRT_LINKAGE dynamic)
+set(VCPKG_LIBRARY_LINKAGE static)
+set(VCPKG_CMAKE_SYSTEM_NAME Android)
+set(VCPKG_CMAKE_SYSTEM_VERSION 34)
+set(VCPKG_MAKE_BUILD_TRIPLET "--host=aarch64-linux-android")
+set(VCPKG_CMAKE_CONFIGURE_OPTIONS -DANDROID_ABI=arm64-v8a)
+
+set(VCPKG_C_FLAGS "-fsanitize=hwaddress -fno-omit-frame-pointer -mllvm -hwasan-globals=0")
+set(VCPKG_CXX_FLAGS "${VCPKG_C_FLAGS}")
+set(VCPKG_LINKER_FLAGS "-fsanitize=hwaddress")
